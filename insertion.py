8478b352c4489c9b64287cf065ef0cd6ad69fa26
nilsu melis sönmez <80063055+snilsumelis@users.noreply.github.com>
2024-01-15 22:22:14 +0300

    Create insertion.py

---
+++ insertion.py
@@ -0,0 +1,18 @@
+def insertion_sort(arr):
+    
+    n = len(arr)
+    
+    for i in range(1,n):
+        key = arr[i]
+        j = i-1
+        
+        while(j>=0 and arr[j]>key):
+            arr[j+1] = arr[j]
+            j=j-1
+        arr[j + 1] = key              
+        
+myArray = [22,27,16,2,18,6]
+insertion_sort(myArray)
+
+print(myArray)
+
